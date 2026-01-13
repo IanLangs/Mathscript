4324b5cd928c4770d809791ed7a9de8062cb31f9
root <supergachaworld4@gmail.com>
2026-01-13 14:52:10 -0300

    1.1.1 2

--- src/node.js
+++ src/node.js
@@ -3,4 +3,4 @@ import T from './transpile.js'
 import fs from 'fs'
 const __code__ = fs.readFileSync(process.argv[2], 'utf-8')
 
-eval(T.transpile(__code__, T.Consts))
+eval(T.transpile(__code__, ...(T.Consts)))
--- src/transpile.js
+++ src/transpile.js
@@ -2,7 +2,7 @@ const Consts = [
     [/::\s*[^\s\(\)]*/g, ""],
     [/mut\s+([a-zA-Z_]\w*)\s*=\s*(.+)/g, "let $1= (()=>{let v=$2; return {get:()=>v,set:n=>v=n}})()"],
     [/immut\s+([a-zA-Z_]\w*)/g, "delete $1.set"],
-    [/using\(/g, "require("],
+    ["using", "require"],
     [/fn/g, "function"],
 ]
 
